b8f761334f893e4b57ce844ba98d13fc4d272f26
unknown <mtn@.(none)>
2011-10-18 11:07:36 +0200

    ex 2.2.3 relfection working..

--- Framework/Renderer.cpp
+++ Framework/Renderer.cpp
@@ -96,12 +96,32 @@ Vector4 Renderer::traceColor(Ray ray, Scene* scene, unsigned int recDepth) {
 
 	Vector4 color(0,0,0);
 	IntersectionData* iData = scene->intersect(ray);
-	
+	double reflectionPercentage;
 	if (iData) { // sucessful intersection test
 
-	  //=== EXERCISE 2.2.3 ===
-
+		//=== EXERCISE 2.2.3 ===
+		reflectionPercentage=iData->reflectionPercentage;
 		color = m_shader->shade(iData, scene);
+		color = color * (1-reflectionPercentage);
+		
+        Vector3 N = iData->normal;
+		Vector3 L = ray.direction;
+		Vector3 R = ((N * N.dot(L) * 2) - L).normalize();
+				
+			 
+		if (recDepth < m_recursionDepth) 
+		{
+			
+			
+				Ray reflected = Ray(iData->position,R);
+				reflected.min_t = reflected.epsilon_t;
+				color += traceColor(reflected,scene,recDepth+1)*reflectionPercentage;
+			
+                        
+		}
+
+
+
     }
 	else {
 		color = scene->getBackground(); // background color
